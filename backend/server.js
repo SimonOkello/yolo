91236b687d3db1fcd203944cbbcecd8a8973fc28
SimonOkello <simonokello93@gmail.com>
2023-10-08 11:49:29 +0300

    add  .gitignore & .dockerignore files

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,13 @@ const upload = multer();
 const productRoute = require('./routes/api/productRoute');
 
 // Connecting to the Database
-let mongodb_url = 'mongodb://localhost/';
-let dbName = 'yolomy';
+// let mongodb_url = 'mongodb://localhost/';
+let mongodb_url = 'mongodb://mongo:27017/yolomy';
+// let dbName = 'yolomy';
 
 // define a url to connect to the database
-const MONGODB_URI = process.env.MONGODB_URI || mongodb_url + dbName
+// const MONGODB_URI = process.env.MONGODB_URI || mongodb_url + dbName
+const MONGODB_URI = process.env.MONGODB_URI || mongodb_url
 mongoose.connect(MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true  } )
 let db = mongoose.connection;
 
